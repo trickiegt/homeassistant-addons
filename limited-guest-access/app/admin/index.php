9a5d0895e62c56ecf5e24eea284c1abf9993a91d
trickiegt <trickiegt@gmail.com>
2022-07-15 21:27:14 -0400

    Update index.php
    Allow start time from yesterday -- fixes my issues of 8pm local being seen as the next day in UI

--- limited-guest-access/app/admin/index.php
+++ limited-guest-access/app/admin/index.php
@@ -364,7 +364,7 @@ if (isset($_REQUEST['action']) && ($_REQUEST['action'] == 'addAction' || $_REQUE
                    class="dateinput"
                    name="valid_from_date"
                    type="date"
-                   min="<?= date('Y-m-d',time())?>"
+                   min="<?= date("Y-m-d", strtotime("yesterday"))?>"
                    value="<?= date('Y-m-d',time())?>"
                    onchange="validateDates();(function(){
                        document.querySelector('#expiry_time_date').min = document.querySelector('#valid_from_date').value;
