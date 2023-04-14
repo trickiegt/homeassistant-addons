a3c5acd3be0415f26834437b531906a590ed6a2d
trickiegt <trickiegt@gmail.com>
2023-04-14 13:57:40 -0400

    Update run.sh

--- limited-guest-access/run.sh
+++ limited-guest-access/run.sh
@@ -2,7 +2,6 @@
 chmod 777 /data/
 chmod 644 /data/options.json
 mkdir -p /share/limited-guest-access/data
-ln -sf /share/limited-guest-access/data /data
 is_ssl_active=$(cat /data/options.json |jq .activate_tls)
 if [ $is_ssl_active = true ]; then
     if test -f "/ssl/privkey.pem"; then
